8452431e5aecbd5ab15d69c33d9fd1b2737d5d84
bogbu <glutamine082442@gmail.com>
2024-09-25 15:12:30 +0900

    first commit

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import SuccessButton from '$lib/success-button.svelte';
+	import DangerButton from '$lib/danger-button.svelte';
+
+	let count = 0;
+
+	// actions
+	const increment = () => count++;
+	const decrement = () => count--;
+	const reset = () => (count = 0);
+</script>
+
+<div>
+	test section
+</div>
